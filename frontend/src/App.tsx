636d144e24a38c0d5c1908c3691ef1c9619f52e0
wai81 <50743966+wai81@users.noreply.github.com>
2024-09-12 11:07:54 +0300

    added in frontend axios updated index.html create first hook in  App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,27 @@
 import './App.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+    const [organizations, setOrganizations] = useState([]);
 
-  return (
-    <div className="App">
-      <h1>РУП "Гродненское агентство по государственной регистрации и земельному кадастру"</h1>
-    </div>
-  )
+    useEffect(() => {
+        axios.get('http://localhost:8001/organizations').then(response => {
+            setOrganizations(response.data.items)
+        })
+    }, [])
+    console.log(organizations)
+    return (
+        <div>
+            <h1>РУП "Гродненское агентство по государственной регистрации и земельному кадастру"</h1>
+            <ul>
+                {organizations.map((organization:any)=>(
+                    <li key={organization.id}>{organization.title}</li>
+                ))}
+
+            </ul>
+        </div>
+    )
 }
 
 export default App
