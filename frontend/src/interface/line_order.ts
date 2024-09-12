6395790246bd04a75e84458a76bfc1a0fd30a116
wai81 <50743966+wai81@users.noreply.github.com>
2024-09-12 21:02:00 +0300

    added interface line_order

---
+++ frontend/src/interface/line_order.ts
@@ -0,0 +1,11 @@
+export interface LineOrder {
+    id: string
+    order_number: string
+    order_create_date: string
+    costumer_contact_phone: string
+    organization_id: number
+    is_completed: boolean
+    created_at: string
+    row_num: number
+  }
+  
