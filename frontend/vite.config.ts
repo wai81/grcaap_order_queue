433f88db27e3e0fad7054423d11307c06363770d
v.tarasevich <vitja.tarasevich@bk.ru>
2024-09-18 09:52:59 +0300

    update vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: '0.0.0.0',
     port: 3000
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 7000,
   },
   plugins: [
       react(),
