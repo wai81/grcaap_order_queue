2a431753ddc5ef6e07f3bc2c0cc91ed8cc391b36
wai81 <50743966+wai81@users.noreply.github.com>
2024-10-30 15:51:19 +0300

    updated dashboard app added router

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -7,26 +7,30 @@ import { CreateOrderLine } from './pages/lineOrders/create'
 import { authProvider } from './providers/auth-provider'
 import { Login } from './pages/login'
 import { Header } from './components/header'
-
+import { BrowserRouter } from 'react-router-dom'
+import routerProvider from "@refinedev/react-router-v6";
 
 
 function App() {
 
 
   return (
-    <Refine
-      dataProvider={dataProvider}
-      authProvider={authProvider}
-    >
-      <Authenticated key="protected" fallback={<div>Вы не авторизованы <Login /></div>}>
-        <Header />
-        {/* <WelcomePage /> */}
-        {/* <ShowLineOrder /> */}
-        {/* <EditLineOrder /> */}
-        < ListLineOrders />
-        {/* <CreateOrderLine /> */}
-      </Authenticated>
-    </Refine>
+    <BrowserRouter>
+      <Refine
+        dataProvider={dataProvider}
+        authProvider={authProvider}
+        routerProvider={routerProvider}
+      >
+        <Authenticated key="protected" fallback={<div>Вы не авторизованы <Login /></div>}>
+          <Header />
+          {/* <WelcomePage /> */}
+          {/* <ShowLineOrder /> */}
+          {/* <EditLineOrder /> */}
+          < ListLineOrders />
+          {/* <CreateOrderLine /> */}
+        </Authenticated>
+      </Refine>
+    </BrowserRouter>
   )
 }
 
